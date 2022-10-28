11b7921d7119fc917bf98eb38b83746b23090e6b
Dmitry Bratsun <dmitry.bratsun@gmail.com>
2022-10-28 11:23:04 +0300

    clean project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { KonvaModule } from './konva/konva.module';
-import { KonvaService } from './konva/konva.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, KonvaModule],
-  // providers: [KonvaService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
